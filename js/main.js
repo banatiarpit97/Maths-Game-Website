901c8ac7d385106524d5c032d17cbf7f5a3596d1
Arpit <banatiarpit97@yahoo.co.in>
2017-09-24 11:09:35 +0530

    validations in dialog box

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ angular.module('mathsGame',['ngMaterial'])
 	var timeLeftCopy;
 	var wrongGuesses = 0;
 
+
 				 $scope.range = function(ev) {
 						 $mdDialog.show({
 								 controller: DialogController,
@@ -27,11 +28,89 @@ angular.module('mathsGame',['ngMaterial'])
 								 parent: angular.element(document.body),
 								 targetEvent: ev,
 								 clickOutsideToClose:true// Only for -xs, -sm breakpoints.
-						 })
+						 });
 
 				 };
 
 				 function DialogController($scope, $mdDialog) {
+        //    if(sessionStorage.startNumber && sessionStorage.endNumber){
+        //      console.log('aa');
+        //      console.log(sessionStorage.startNumber)
+        //    $scope.startNumber = (sessionStorage.startNumber);
+        //    $(".endNumber").html(sessionStorage.endNumber);
+        //  }
+        //    else{
+        //      $(".startNumber").html(1);
+        //      $(".endNumber").html(10);
+        //    }
+
+        $scope.timeError = function(){
+          $scope.timeMessage = "";
+          if(isNaN($scope.time) || ($scope.time == null) ){
+            $scope.timeMessage += "Enter time in seconds";
+            $(".timeMessage").html($scope.timeMessage);
+            $(".timeError").removeClass("hidden");
+            $scope.timeokay = 0;
+          }
+          else if($scope.time <= 0){
+            $scope.timeMessage += "Enter value greater than 0";
+            $(".timeMessage").html($scope.timeMessage);
+            $(".timeError").removeClass("hidden");
+            $scope.timeokay = 0;
+          }
+          else {
+            $scope.timeokay = 1;
+          }
+        }
+           $scope.rangeError = function(){
+             $scope.rangeMessage1 = "";
+             $scope.rangeMessage2 = "";
+             if(isNaN($scope.startNumber) || ($scope.startNumber == null) ){
+               $scope.rangeMessage1 += "Enter a Number";
+               $(".rangeMessage1").html($scope.rangeMessage1);
+               $(".rangeError1").removeClass("hidden");
+               $scope.okay1 = 0;
+             }
+             else if($scope.startNumber <= 0){
+               $scope.rangeMessage1 += "Enter a Number greater than 0";
+               $(".rangeMessage1").html($scope.rangeMessage1);
+               $(".rangeError1").removeClass("hidden");
+               $scope.okay1 = 0;
+             }
+             else if (!isNaN($scope.startNumber)) {
+
+               $(".rangeMessage1").html($scope.rangeMessage1);
+               $(".rangeError1").addClass("hidden");
+               $scope.okay1 = 1;
+             }
+             if(isNaN($scope.endNumber) || ($scope.endNumber == null) ){
+               $scope.rangeMessage2 += "Enter a Number";
+               $(".rangeMessage2").html($scope.rangeMessage2);
+               $(".rangeError2").removeClass("hidden");
+               $scope.okay2 = 0;
+
+             }
+             else if($scope.endNumber <= 0){
+               $scope.rangeMessage2 += "Enter a Number greater than 0";
+               $(".rangeMessage2").html($scope.rangeMessage2);
+               $(".rangeError2").removeClass("hidden");
+               $scope.okay2 = 0;
+             }
+             else if (!isNaN($scope.endNumber)) {
+               $(".rangeMessage2").html($scope.rangeMessage2);
+               $(".rangeError2").addClass("hidden");
+               $scope.okay2 = 1;
+             }
+             if(($scope.okay1 + $scope.okay2 == 2) && ($scope.endNumber <= $scope.startNumber)){
+               $(".greaterMessage").removeClass("hidden");
+               $scope.okay3 = 0;
+             }
+             else{
+               $(".greaterMessage").addClass("hidden");
+               $scope.okay3 = 1;
+             }
+
+           }
 						 $scope.close = function() {
 								 $mdDialog.cancel();
 						 };
@@ -39,6 +118,10 @@ angular.module('mathsGame',['ngMaterial'])
 								 $mdDialog.cancel();
 						 };
 						 $scope.confirmRange = function() {
+               $scope.rangeError();
+
+               if($scope.okay1 + $scope.okay2 + $scope.okay3 == 3)
+                {
 								 sessionStorage.startNumber = Number($scope.startNumber);
 								 sessionStorage.endNumber = Number($scope.endNumber);
 								 minNumber =  $scope.startNumber;
@@ -47,16 +130,20 @@ angular.module('mathsGame',['ngMaterial'])
 								 	$(".score_value").html(score);
 								 	generateQuesAns();
 								 $scope.close();
+              }
 						 }
 						 $scope.confirmTime = function() {
+               $scope.timeError();
+               if($scope.timeokay == 1){
 								 sessionStorage.time = $scope.time;
 								 timeLeft = $scope.time;
 								 timeLeftCopy = timeLeft;
 								 	score = 0;
 								 	$(".score_value").html(score);
-                                    wrongGuesses = 0;
+                  wrongGuesses = 0;
 								 	generateQuesAns();
 								 $scope.close();
+               }
 
 						 }
 
